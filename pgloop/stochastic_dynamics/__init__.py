ed709c512c5a568e93054f60838d5335efbb4db8
Siqi Tang <165524145+ResearchGeekSQ@users.noreply.github.com>
2026-06-06 00:02:44 +0800

    refactor(pinn): export PINN modules in __init__.py

--- pgloop/stochastic_dynamics/__init__.py
+++ pgloop/stochastic_dynamics/__init__.py
@@ -17,9 +17,16 @@ from pgloop.stochastic_dynamics.eval import (
 )
 from pgloop.stochastic_dynamics.fokker_planck import FokkerPlanck1DSolver, FokkerPlanck2DSolver
 
+from pgloop.stochastic_dynamics.acr_sampler import AdaptiveCollocationSampler
+from pgloop.stochastic_dynamics.pinn_trainer import StiffPINNTrainer
+from pgloop.stochastic_dynamics.stiff_pinn import StiffBoundaryPINN
+
 __all__ = [
     "FokkerPlanck1DSolver",
     "FokkerPlanck2DSolver",
+    "StiffBoundaryPINN",
+    "AdaptiveCollocationSampler",
+    "StiffPINNTrainer",
     "l2_pdf_error",
     "kl_divergence",
     "conservation_error",
